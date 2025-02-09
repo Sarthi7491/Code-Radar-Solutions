3834d24f52dc418c7fa47f4ef92f4e3b37d8564e
Sarthi Mahajan <sarthimahajan18@gmail.com>
2025-02-09 19:49:44 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,6 +1,22 @@
+// #include <stdio.h>
+// int main(){
+//    int x,y,z;
+//    scanf("%d%d%d",&x, &y, &z);
+
+//     if 
+// }
+
 #include <stdio.h>
-int main(){
-   int x,y,z;
-   scanf("%d%d%d",&x, &y, &z);
-     
+
+int main() {
+    float a, b, c;
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("The sides %.2f, %.2f, and %.2f form a valid triangle.\n", a, b, c);
+    } else {
+        printf("The sides %.2f, %.2f, and %.2f do not form a valid triangle.\n", a, b, c);
+    }
+
+    return 0;
 }
