02c99c671d477bef9f7839d90d9e0da82b91bc57
Sarthi Mahajan <sarthimahajan18@gmail.com>
2025-02-26 22:34:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,33 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main() 
+{
+    int x,y,z;
+    scanf("%d %d %d",&x, &y, &z);
 
-int main() {
-    char operator;
-    double num1, num2;
     
-    // Read input
-    scanf("%c %lf %lf", &operator, &num1, &num2);
-    
-    switch (operator) {
-        case '+':
-            printf("%.2lf\n", num1 + num2);
-            break;
-        case '-':
-            printf("%.2lf\n", num1 - num2);
-            break;
-        case '*':
-            printf("%.2lf\n", num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0.0)
-                printf("%.2lf\n", num1 / num2);
-            else
-                printf("Error\n");
-            break;
-        default:
-            printf("Error\n");
-            break;
-    }
-    
-    return 0;
 }
